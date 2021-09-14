b7c0b84e71ac9f069b6fbaac9af741b3031e20d1
Sahil <sahkumar@adobe.com>
2021-09-14 20:52:52 +0530

    Updated samples for the Adobe PDF Services SDK 2.1.0

---
+++ ExtractTextTableInfoWithFiguresTablesRenditionsFromPDF/Program.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Copyright 2020 Adobe
+ * All Rights Reserved.
+ *
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in 
+ * accordance with the terms of the Adobe license agreement accompanying 
+ * it. If you have received this file from a source other than Adobe, 
+ * then your use, modification, or distribution of it requires the prior 
+ * written permission of Adobe.
+ */
+using System.IO;
+using System;
+using System.Collections.Generic;
+using log4net.Repository;
+using log4net.Config;
+using log4net;
+using System.Reflection;
+using Adobe.PDFServicesSDK;
+using Adobe.PDFServicesSDK.auth;
+using Adobe.PDFServicesSDK.pdfops;
+using Adobe.PDFServicesSDK.io;
+using Adobe.PDFServicesSDK.exception;
+using Adobe.PDFServicesSDK.options.extractpdf;
+
+/// <summary>
+/// This sample illustrates how to extract a PDF file.
+/// <para/>
+/// Note that extracting a PDF file results in a ZIP archive containing texta and tables
+/// <para/>
+/// Refer to README.md for instructions on how to run the samples.
+/// </summary>
+namespace ExtractTextTableInfoWithFiguresTablesRenditionsFromPDF
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        static void Main()
+        {
+            //Configure the logging
+            ConfigureLogging();
+            try
+            {
+                // Initial setup, create credentials instance.
+                Credentials credentials = Credentials.ServiceAccountCredentialsBuilder()
+                                .FromFile(Directory.GetCurrentDirectory() + "/pdfservices-api-credentials.json")
+                                .Build();
+
+                //Create an ExecutionContext using credentials and create a new operation instance.
+                ExecutionContext executionContext = ExecutionContext.Create(credentials);
+                ExtractPDFOperation extractPdfOperation = ExtractPDFOperation.CreateNew();
+
+                // Set operation input from a source file.
+                FileRef sourceFileRef = FileRef.CreateFromLocalFile(@"extractPdfInput.pdf");
+                extractPdfOperation.SetInputFile(sourceFileRef);
+                
+                // Build ExtractPDF options and set them into the operation
+                ExtractPDFOptions extractPdfOptions = ExtractPDFOptions.ExtractPdfOptionsBuilder()
+                        .AddElementsToExtract(new List<ExtractElementType>(new []{ ExtractElementType.TEXT, ExtractElementType.TABLES}))
+                        .AddElementsToExtractRenditions(new List<ExtractRenditionsElementType> (new []{ExtractRenditionsElementType.FIGURES, ExtractRenditionsElementType.TABLES}))
+                        .build();
+                    
+                extractPdfOperation.SetOptions(extractPdfOptions);
+
+
+                // Execute the operation.
+                FileRef result = extractPdfOperation.Execute(executionContext);
+
+                // Save the result to the specified location.
+                result.SaveAs(Directory.GetCurrentDirectory() + "/output/ExtractTextTableInfoWithFiguresTablesRenditionsFromPDF.zip");
+            }
+            catch (ServiceUsageException ex)
+            {
+                log.Error("Exception encountered while executing operation", ex);
+            }
+            catch (ServiceApiException ex)
+            {
+                log.Error("Exception encountered while executing operation", ex);
+            }
+            catch (SDKException ex)
+            {
+                log.Error("Exception encountered while executing operation", ex);
+            }
+            catch (IOException ex)
+            {
+                log.Error("Exception encountered while executing operation", ex);
+            }
+            catch (Exception ex)
+            {
+                log.Error("Exception encountered while executing operation", ex);
+            }
+        }
+
+        static void ConfigureLogging()
+        {
+            ILoggerRepository logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+    }
+}
