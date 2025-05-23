ef6f33562b713a5271ed0f630c515cf021d8b5e9
Pushkar Soni <pushkars@Pushkars-MacBook-Pro-2.local>
2025-05-23 13:20:45 +0530

    sdk v4.3.0 release - import/export samples added

--- ExportPDFFormData/Program.cs
+++ ExportPDFFormData/Program.cs
@@ -1,33 +1,34 @@
-﻿/*
+/*
  * Copyright 2024 Adobe
  * All Rights Reserved.
  *
- * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in 
  * accordance with the terms of the Adobe license agreement accompanying it.
  */
 
+using System;
+using System.IO;
 using System.Reflection;
 using Adobe.PDFServicesSDK;
 using Adobe.PDFServicesSDK.auth;
 using Adobe.PDFServicesSDK.exception;
 using Adobe.PDFServicesSDK.io;
 using Adobe.PDFServicesSDK.pdfjobs.jobs;
-using Adobe.PDFServicesSDK.pdfjobs.parameters;
-using Adobe.PDFServicesSDK.pdfjobs.parameters.pdfwatermark;
 using Adobe.PDFServicesSDK.pdfjobs.results;
 using log4net;
 using log4net.Config;
 using log4net.Repository;
 
 /// <summary>
-/// This sample illustrates how to perform PDF Watermark operation on a PDF file.
-/// Note that PDF Watermark operation on a PDF file results in a PDF file.
+/// This sample demonstrates how to use Adobe PDF Services SDK to extract form data from a PDF file.
+/// The process involves uploading a source PDF, submitting an export form data job, and retrieving the extracted data
+/// in json format.
 /// <para/>
 /// Refer to README.md for instructions on how to run the samples.
 /// </summary>
-namespace PDFWatermarkWithOptions
+namespace ExportPDFFormData
 {
-    internal class Program
+    class Program
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
@@ -42,48 +43,29 @@ namespace PDFWatermarkWithOptions
                     Environment.GetEnvironmentVariable("PDF_SERVICES_CLIENT_ID"),
                     Environment.GetEnvironmentVariable("PDF_SERVICES_CLIENT_SECRET"));
 
+                // Creates a PDF Services instance
                 PDFServices pdfServices = new PDFServices(credentials);
 
                 // Creates an asset(s) from source file(s) and upload
-                Stream sourceFileInputStream = File.OpenRead(@"pdfWatermarkInput.pdf");
-                IAsset inputDocumentAsset =
-                    pdfServices.Upload(sourceFileInputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
+                using Stream inputStream = File.OpenRead(@"exportPdfFormDataInput.pdf");
+                IAsset asset = pdfServices.Upload(inputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
 
-                // Creates a watermark asset from source file(s) and upload
-                Stream watermarkFileInputStream = File.OpenRead(@"watermark.pdf");
-                IAsset watermarkDocumentAsset =
-                    pdfServices.Upload(watermarkFileInputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
-
-                // Watermark pages of the document (as specified by PageRanges).
-                PageRanges pageRangesForPDFWatermark = getPageRangeForPDFWatermark();
-
-                // Creates PDF Watermark appearance option
-                WatermarkAppearance watermarkAppearance = new WatermarkAppearance();
-                watermarkAppearance.SetOpacity(50);
-
-                // Create parameters for the job
-                PDFWatermarkParams pdfWatermarkParams = PDFWatermarkParams.PDFWatermarkParamsBuilder()
-                    .WithPageRanges(pageRangesForPDFWatermark)
-                    .WithWatermarkAppearance(watermarkAppearance)
-                    .Build();
+                // Creates a new job instance
+                ExportPDFFormDataJob exportPDFFormDataJob = new ExportPDFFormDataJob(asset);
 
                 // Submits the job and gets the job result
-                PDFWatermarkJob pdfWatermarkJob =
-                    new PDFWatermarkJob(inputDocumentAsset, watermarkDocumentAsset).SetParams(pdfWatermarkParams);
-                String location = pdfServices.Submit(pdfWatermarkJob);
+                String location = pdfServices.Submit(exportPDFFormDataJob);
+                PDFServicesResponse<ExportPDFFormDataResult> pdfServicesResponse =
+                    pdfServices.GetJobResult<ExportPDFFormDataResult>(location, typeof(ExportPDFFormDataResult));
 
                 // Get content from the resulting asset(s)
-                PDFServicesResponse<PDFWatermarkResult> pdfServicesResponse =
-                    pdfServices.GetJobResult<PDFWatermarkResult>(location, typeof(PDFWatermarkResult));
-
-                // Creating output streams and copying stream asset's content to it
                 IAsset resultAsset = pdfServicesResponse.Result.Asset;
                 StreamAsset streamAsset = pdfServices.GetContent(resultAsset);
 
+                // Creating output streams and copying stream asset's content to it
                 String outputFilePath = CreateOutputFilePath();
                 new FileInfo(Directory.GetCurrentDirectory() + outputFilePath).Directory.Create();
                 Stream outputStream = File.OpenWrite(Directory.GetCurrentDirectory() + outputFilePath);
-
                 streamAsset.Stream.CopyTo(outputStream);
                 outputStream.Close();
             }
@@ -118,19 +100,7 @@ namespace PDFWatermarkWithOptions
         private static String CreateOutputFilePath()
         {
             String timeStamp = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH'-'mm'-'ss");
-            return ("/output/watermark" + timeStamp + ".pdf");
-        }
-
-        private static PageRanges getPageRangeForPDFWatermark()
-        {
-            // Specify pages for deletion
-            PageRanges pageRangeForPDFWatermark = new PageRanges();
-            // Add page 1
-            pageRangeForPDFWatermark.AddSinglePage(1);
-
-            // Add pages 3 to 4
-            pageRangeForPDFWatermark.AddRange(3, 4);
-            return pageRangeForPDFWatermark;
+            return ("/output/ExportPDFFormData/getFormData" + timeStamp + ".json");
         }
     }
 }
--- ImportPDFFormData/Program.cs
+++ ImportPDFFormData/Program.cs
@@ -1,33 +1,36 @@
-﻿/*
+/*
  * Copyright 2024 Adobe
  * All Rights Reserved.
  *
- * NOTICE: Adobe permits you to use, modify, and distribute this file in
+ * NOTICE: Adobe permits you to use, modify, and distribute this file in 
  * accordance with the terms of the Adobe license agreement accompanying it.
  */
 
+using System;
+using System.IO;
 using System.Reflection;
 using Adobe.PDFServicesSDK;
 using Adobe.PDFServicesSDK.auth;
 using Adobe.PDFServicesSDK.exception;
 using Adobe.PDFServicesSDK.io;
 using Adobe.PDFServicesSDK.pdfjobs.jobs;
-using Adobe.PDFServicesSDK.pdfjobs.parameters;
-using Adobe.PDFServicesSDK.pdfjobs.parameters.pdfwatermark;
+using Adobe.PDFServicesSDK.pdfjobs.parameters.importpdfformdata;
 using Adobe.PDFServicesSDK.pdfjobs.results;
 using log4net;
 using log4net.Config;
 using log4net.Repository;
+using Newtonsoft.Json.Linq;
 
 /// <summary>
-/// This sample illustrates how to perform PDF Watermark operation on a PDF file.
-/// Note that PDF Watermark operation on a PDF file results in a PDF file.
+/// This sample demonstrates how to use Adobe PDF Services SDK to import form data into a PDF form.
+/// The process involves uploading a source PDF, providing form data in JSON format, and submitting
+/// an import form data job.
 /// <para/>
 /// Refer to README.md for instructions on how to run the samples.
 /// </summary>
-namespace PDFWatermarkWithOptions
+namespace ImportPDFFormData
 {
-    internal class Program
+    class Program
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
@@ -42,48 +45,44 @@ namespace PDFWatermarkWithOptions
                     Environment.GetEnvironmentVariable("PDF_SERVICES_CLIENT_ID"),
                     Environment.GetEnvironmentVariable("PDF_SERVICES_CLIENT_SECRET"));
 
+                // Creates a PDF Services instance
                 PDFServices pdfServices = new PDFServices(credentials);
 
                 // Creates an asset(s) from source file(s) and upload
-                Stream sourceFileInputStream = File.OpenRead(@"pdfWatermarkInput.pdf");
-                IAsset inputDocumentAsset =
-                    pdfServices.Upload(sourceFileInputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
-
-                // Creates a watermark asset from source file(s) and upload
-                Stream watermarkFileInputStream = File.OpenRead(@"watermark.pdf");
-                IAsset watermarkDocumentAsset =
-                    pdfServices.Upload(watermarkFileInputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
-
-                // Watermark pages of the document (as specified by PageRanges).
-                PageRanges pageRangesForPDFWatermark = getPageRangeForPDFWatermark();
-
-                // Creates PDF Watermark appearance option
-                WatermarkAppearance watermarkAppearance = new WatermarkAppearance();
-                watermarkAppearance.SetOpacity(50);
+                using Stream inputStream = File.OpenRead(@"importPdfFormDataInput.pdf");
+                IAsset asset = pdfServices.Upload(inputStream, PDFServicesMediaType.PDF.GetMIMETypeValue());
 
                 // Create parameters for the job
-                PDFWatermarkParams pdfWatermarkParams = PDFWatermarkParams.PDFWatermarkParamsBuilder()
-                    .WithPageRanges(pageRangesForPDFWatermark)
-                    .WithWatermarkAppearance(watermarkAppearance)
+                var formData = new JObject(
+                    new JProperty("option_two", "Yes"),
+                    new JProperty("option_one", "Yes"),
+                    new JProperty("name", "sufia"),
+                    new JProperty("option_three", "Off"),
+                    new JProperty("age", "25"),
+                    new JProperty("favorite_movie", "Star Wars Again")
+                );
+
+                ImportPDFFormDataParams importParams = ImportPDFFormDataParams.ImportPDFFormDataParamsBuilder()
+                    .WithJsonFormFieldsData(formData)
                     .Build();
 
+                // Creates a new job instance
+                ImportPDFFormDataJob importPDFFormDataJob = new ImportPDFFormDataJob(asset);
+                importPDFFormDataJob.SetParams(importParams);
+
                 // Submits the job and gets the job result
-                PDFWatermarkJob pdfWatermarkJob =
-                    new PDFWatermarkJob(inputDocumentAsset, watermarkDocumentAsset).SetParams(pdfWatermarkParams);
-                String location = pdfServices.Submit(pdfWatermarkJob);
+                String location = pdfServices.Submit(importPDFFormDataJob);
+                PDFServicesResponse<ImportPDFFormDataResult> pdfServicesResponse =
+                    pdfServices.GetJobResult<ImportPDFFormDataResult>(location, typeof(ImportPDFFormDataResult));
 
                 // Get content from the resulting asset(s)
-                PDFServicesResponse<PDFWatermarkResult> pdfServicesResponse =
-                    pdfServices.GetJobResult<PDFWatermarkResult>(location, typeof(PDFWatermarkResult));
-
-                // Creating output streams and copying stream asset's content to it
                 IAsset resultAsset = pdfServicesResponse.Result.Asset;
                 StreamAsset streamAsset = pdfServices.GetContent(resultAsset);
 
+                // Creating output streams and copying stream asset's content to it
                 String outputFilePath = CreateOutputFilePath();
                 new FileInfo(Directory.GetCurrentDirectory() + outputFilePath).Directory.Create();
                 Stream outputStream = File.OpenWrite(Directory.GetCurrentDirectory() + outputFilePath);
-
                 streamAsset.Stream.CopyTo(outputStream);
                 outputStream.Close();
             }
@@ -118,19 +117,7 @@ namespace PDFWatermarkWithOptions
         private static String CreateOutputFilePath()
         {
             String timeStamp = DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH'-'mm'-'ss");
-            return ("/output/watermark" + timeStamp + ".pdf");
-        }
-
-        private static PageRanges getPageRangeForPDFWatermark()
-        {
-            // Specify pages for deletion
-            PageRanges pageRangeForPDFWatermark = new PageRanges();
-            // Add page 1
-            pageRangeForPDFWatermark.AddSinglePage(1);
-
-            // Add pages 3 to 4
-            pageRangeForPDFWatermark.AddRange(3, 4);
-            return pageRangeForPDFWatermark;
+            return ("/output/ImportPDFFormData/setFormData" + timeStamp + ".pdf");
         }
     }
 }
